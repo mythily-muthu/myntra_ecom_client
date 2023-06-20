902a89624c59ac7d6440c82cbddd912b3828a0b7
mythily-muthu <mythilimuthu18@gmail.com>
2023-06-20 11:09:01 +0530

    Created pages and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="">
-      <h1>sdf</h1>
+    <div className="flex flex-col w-full h-full">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product/:productid" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="flex w-full h-8 bg-orange-200 shadow-md"></div>;
+};
+
+export default Navbar;
